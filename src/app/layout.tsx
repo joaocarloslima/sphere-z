41767461c5bc624ffd1277c5bc2170d0784e5653
joaocarloslima <joaocarloslima@me.com>
2024-08-26 15:59:02 -0300

    auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,14 +18,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider
-              attribute="class"
-              defaultTheme="dark"
-              enableSystem
-              disableTransitionOnChange
-            >
-          {children}
-        </ThemeProvider>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+        {children}
+      </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,76 +1,42 @@
+import LoginForm from "@/components/home/login-form";
+import RegisterForm from "@/components/home/register-form";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-between">
-      <nav className="absolute right-2 top-2">
+    <main className="flex min-h-screen items-center justify-between ">
+      <nav className="absolute right-1 top-1">
         <ModeToggle />
       </nav>
 
-      <aside className="hidden lg:block">
-        <Image src="/login.png" alt="imagem de login" width={554} height={832} />
+      <aside className="h-screen hidden lg:block ">
+        <Image src="/login.png" alt="login" width={554} height={832}/>
       </aside>
 
-      <section className="flex flex-col items-center w-full gap-4">
-        <svg width="75" height="75" viewBox="0 0 75 75" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <circle cx="37.5" cy="37.5" r="37.5" fill="#E11D48"/>
-        </svg>
+      <div className="flex flex-col items-center gap-10 m-auto">
+        <div className="size-[75px] bg-rose-600 rounded-full m-auto"></div>
         <h1 className="text-4xl font-extrabold uppercase">Sphere</h1>
-
-        <Tabs>
-          <TabsList defaultValue="entrar" className="min-w-[200px]">
+        
+        <Tabs defaultValue="entrar" className="w-[400px]">
+          <TabsList className="grid w-full grid-cols-2">
             <TabsTrigger value="entrar">entrar</TabsTrigger>
             <TabsTrigger value="registrar">registrar</TabsTrigger>
           </TabsList>
-
+          
           <TabsContent value="entrar">
-            <form action="" className="space-y-4">
-              <div>
-                <Label htmlFor="email">email</Label>
-                <Input id="email" />
-              </div>
-              <div>
-                <Label htmlFor="senha">senha</Label>
-                <Input id="senha" type="password"/>
-              </div>              
-
-             <Button type="submit">entrar</Button>
-            </form>
+            <LoginForm />
           </TabsContent>
 
           <TabsContent value="registrar">
-          <form action="" className="space-y-4">
-              <div>
-                <Label htmlFor="nome">nome</Label>
-                <Input id="nome" />
-              </div>
-              <div>
-                <Label htmlFor="bio">bio</Label>
-                <Input id="bio" />
-              </div>
-              <div>
-                <Label htmlFor="email">email</Label>
-                <Input id="email" />
-              </div>
-              <div>
-                <Label htmlFor="senha">senha</Label>
-                <Input id="senha" type="password"/>
-              </div>              
-
-             <Button type="submit">criar conta</Button>
-            </form>
+            <RegisterForm />
           </TabsContent>
-
         </Tabs>
 
-
-      </section>
-
+      </div>
+      
     </main>
   );
 }
